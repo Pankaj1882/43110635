6c670194fd641e3917a3593529b2dbac665c9f9e
Pankaj1882 <pankajvinod.150605@gmail.com>
2025-08-18 13:52:57 +0530

    Add files via upload

---
+++ placement/script.js
@@ -0,0 +1,27 @@
+const box1 = document.getElementById("box1");
+
+    const marginSlider = document.getElementById("marginSlider");
+    const borderSlider = document.getElementById("borderSlider");
+    const paddingSlider = document.getElementById("paddingSlider");
+
+    const marginValue = document.getElementById("marginValue");
+    const borderValue = document.getElementById("borderValue");
+    const paddingValue = document.getElementById("paddingValue");
+
+    marginSlider.addEventListener("input", () => {
+      const value = marginSlider.value + "px";
+      box1.style.margin = value;
+      marginValue.innerText = value;
+    });
+
+    borderSlider.addEventListener("input", () => {
+      const value = borderSlider.value + "px";
+      box1.style.borderWidth = value;
+      borderValue.innerText = value;
+    });
+
+    paddingSlider.addEventListener("input", () => {
+      const value = paddingSlider.value + "px";
+      box1.style.padding = value;
+      paddingValue.innerText = value;
+    });
